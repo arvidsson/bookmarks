bbcda69462e727eb526f67f04ec6029e83bb9f0c
arvidsson <arvidsson@gmail.com>
2023-06-10 21:42:35 +0200

    registered bookmark model in admin

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Bookmark
+
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "url", "description", "read_later", "is_public", "tag_list", "created_at")
+    readonly_fields = ("created_at",)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Bookmark, BookmarkAdmin)
