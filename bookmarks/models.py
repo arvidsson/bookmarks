69f0193f6b75a7f98e7ff9ac04ed37fe018fca40
arvidsson <arvidsson@gmail.com>
2023-06-10 21:36:54 +0200

    added bookmark model and django-taggit

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from taggit.managers import TaggableManager
 
 class User(AbstractUser):
     pass
+
+class Bookmark(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=256)
+    url = models.URLField()
+    description = models.TextField(blank=True, max_length=2048)
+    read_later = models.BooleanField(default=False)
+    is_public = models.BooleanField(default=False)
+    tags = TaggableManager(blank=True)
--- project/settings.py
+++ project/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggit',
     'bookmarks',
 ]
 
 AUTH_USER_MODEL = 'bookmarks.User'
+
+TAGGIT_CASE_INSENSITIVE = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
