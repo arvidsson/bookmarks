3bc7ec8e9dfa77142866cf71a38171df6e6a6a2b
arvidsson <arvidsson@gmail.com>
2023-06-10 20:27:54 +0200

    added custom user model

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookmarks',
 ]
+
+AUTH_USER_MODEL = 'bookmarks.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
