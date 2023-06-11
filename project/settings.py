156b51586ab8a1915f3d865b0a9c2e46ddceb719
arvidsson <arvidsson@gmail.com>
2023-06-11 09:58:00 +0200

    added login and logout pages

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bookmarks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'widget_tweaks',
     'taggit',
-    'bookmarks',
 ]
 
 AUTH_USER_MODEL = 'bookmarks.User'
