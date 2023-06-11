f8c4d013d902535245c9841255b7d65e0e9b7ea8
arvidsson <arvidsson@gmail.com>
2023-06-11 10:10:10 +0200

    added index route

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.contrib.auth import views
 
 urlpatterns = [
+    path('', include("bookmarks.urls")),
     path("admin/", admin.site.urls),
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
